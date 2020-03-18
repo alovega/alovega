c4615b2d5494ce7de9f3caaa1487be83ec6f7564
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-03-18 12:43:23 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -1,10 +1,10 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\dailyidea.com")
+repo = git.Repo(r"C:\Users\FP IT\Projects\helaplan-v3")
 mock_repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
 
 importer = Importer([repo], mock_repo)
 importer.set_author(['alovegakevin@gmail.com','kevin.lugaziwa@financeplan.biz'])
 
-importer.import_repository()
+print("dipym")
