cd4e5f098e308de56035a02e880a38ff136c7f2f
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-03-24 17:54:50 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -6,4 +6,4 @@ mock_repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
 
 importer = Importer([repo], mock_repo)
 importer.set_author(['alovegakevin@gmail.com','kevin.lugaziwa@financeplan.biz'])
-print("xwldp")
+print("sebke")
