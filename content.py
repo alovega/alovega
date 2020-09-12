7d9bfac7dc68083a36c695c018bb6f9acadbe66a
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-09-12 16:21:32 +0000

    add code in files types: ,.vue,.json,.lock remove code in files types: .vue,.json,.lock

--- content.py
+++ content.py
@@ -1,7 +1,7 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\hamachi")
+repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\dailyidea.com")
 mock_repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
 
 importer = Importer([repo], mock_repo)
