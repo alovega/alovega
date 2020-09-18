3bc608af2d5b2c84102e027edb328bc5bddf574c
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-09-18 15:33:35 +0300

    adding contributions

--- scripts.py
+++ scripts.py
@@ -1,7 +1,7 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\dailyidea.com")
+repo = git.Repo(r"C:\Users\FP IT\Projects\helaplan-v3")
 mock_repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
 
 importer = Importer([repo], mock_repo)
