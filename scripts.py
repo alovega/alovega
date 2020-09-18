7ca1bcbab4dcfbe50d70a302bbd8947bbad85631
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-09-18 15:40:12 +0300

    adding contributions

--- scripts.py
+++ scripts.py
@@ -1,8 +1,9 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo(r"C:\Users\FP IT\Projects\helaplan-v3")
-mock_repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
+
+repo = git.Repo(r"C:\Users\FP IT\Projects\Personal\alovega")
+mock_repo = git.Repo(r"C:\Users\FP IT\Projects\helaplan-v3")
 
 importer = Importer([repo], mock_repo)
 importer.set_author(['alovegakevin@gmail.com','kevin.lugaziwa@financeplan.biz'])
